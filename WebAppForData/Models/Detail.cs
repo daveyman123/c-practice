094017ffb2f08829123c6582b0779d446be59ea8
david graff <djgraff1@cougars.ccis.edu>
2017-12-25 17:48:24 -0600

    first commit

---
+++ WebAppForData/Models/Detail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAppForData.Models
+{
+	public class Detail
+	{
+		[Key]
+		public int DetailID { get; set; }
+
+		public string Description { get; set; }
+
+		[ForeignKey("Order")]
+		public int OrderId { get; set; }
+
+		[ForeignKey("OrderId")]
+		public Order Order { get; set; }
+	}
+}
---
+++ WebAppForData/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppForData.Models
+{
+	public class Order
+	{
+		[Key]
+		public int OrderId { get; set; }
+		public string Name { get; set; }
+		public virtual ICollection<Detail> Details { get; set; }
+
+	}
+}
